32ef666ce44f515ebbc8869118a87448fc550470
xxKiddMdx <zhongpeiyu9166@gmail.com>
2024-05-02 01:18:31 -0400

    Commit

--- src/main/scala/final_project/main.scala
+++ src/main/scala/final_project/main.scala
@@ -1,114 +1,139 @@
-package final_project
+package project_3
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
 import org.apache.spark.graphx._
 import org.apache.spark.storage.StorageLevel
 import org.apache.log4j.{Level, Logger}
-import scala.util.Random
 
-case class MatchedEdge(vertex1: Long, vertex2: Long)
-
-object main {
+object Main {
   val rootLogger = Logger.getRootLogger()
   rootLogger.setLevel(Level.ERROR)
-
   Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
   Logger.getLogger("org.spark-project").setLevel(Level.WARN)
 
-  // Greedy Matching Algorithm
-  def greedyMatching(graphInput: Graph[Int, Int]): RDD[MatchedEdge] = {
-    var activeEdges: RDD[Edge[Int]] = graphInput.edges
-    var selectedEdges = Set[Edge[Int]]()
+  def main(args: Array[String]) {
 
-    def isAnyEdgeActive(edges: RDD[Edge[Int]]): Boolean = edges.count() > 0
-
-    while (isAnyEdgeActive(activeEdges)) {
-      // Select an edge arbitrarily (greedy choice)
-      val selectedEdge = activeEdges.take(1).head
-
-      // Add the selected edge to the matching set
-      selectedEdges += selectedEdge
-
-      // Find the vertices of the selected edge
-      val selectedEdgeVertices = Set(selectedEdge.srcId, selectedEdge.dstId)
-
-      // Remove all edges that share vertices with the selected edge
-      activeEdges = activeEdges.filter(e => !selectedEdgeVertices.contains(e.srcId) && !selectedEdgeVertices.contains(e.dstId))
-    }
-
-    val matchedEdges: RDD[MatchedEdge] = graphInput.edges.sparkContext.parallelize(selectedEdges.toSeq).map(e => MatchedEdge(e.srcId, e.dstId))
-    matchedEdges
-  }
-
-  def lineToCanonicalEdge(line: String): Edge[Int] = {
-    val x = line.split(",")
-    if (x(0).toLong < x(1).toLong)
-      Edge(x(0).toLong, x(1).toLong, 1)
-    else
-      Edge(x(1).toLong, x(0).toLong, 1)
-  }
-
-  def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("Greedy Matching and Verifier")
+    val conf = new SparkConf().setAppName("GraphMatching")
     val sc = new SparkContext(conf)
     val spark = SparkSession.builder.config(conf).getOrCreate()
-    import spark.implicits._
 
-    if (args.length < 2) {
-      println("Usage: MainApp <command> <graph_path> [output_path]")
+    if (args.length == 0) {
+      println("Usage: GraphMatching option = {compute, verify}")
       sys.exit(1)
     }
 
-    val operation = args(0)
-    operation match {
-      case "compute" if args.length == 3 =>
-        val startTimeMillis = System.currentTimeMillis()
-
-        val edges = sc.textFile(args(1)).map { line =>
-          val parts = line.split(",")
-          Edge(parts(0).toLong, parts(1).toLong, 1)
-        }
-        val graph = Graph.fromEdges[Int, Int](edges, defaultValue = 0)
-        val matchedEdges = greedyMatching(graph)
-
-        val endTimeMillis = System.currentTimeMillis()
-        val durationSeconds = (endTimeMillis - startTimeMillis) / 1000
-        println("==================================")
-        println("Greedy Matching algorithm completed in " + durationSeconds + "s.")
-        println("==================================")
-
-        // Save the results
-        val matchedEdgesDF = matchedEdges.toDF("vertex1", "vertex2")
-        matchedEdgesDF.coalesce(1).write.format("csv").mode("overwrite").save(args(2))
-
-      case "verify" if args.length == 3 =>
-        val graphEdges = sc.textFile(args(1)).map(lineToCanonicalEdge)
-        val matchedEdges = sc.textFile(args(2)).map(lineToCanonicalEdge)
-
-        if (matchedEdges.distinct().count() != matchedEdges.count()) {
-          println("The matched edges contain duplications of an edge.")
-          sys.exit(1)
-        }
-
-        if (matchedEdges.intersection(graphEdges).count() != matchedEdges.count()) {
-          println("The matched edges are not a subset of the input graph.")
-          sys.exit(1)
-        }
-
-        val matchedGraph = Graph.fromEdges[Int, Int](matchedEdges, 0, edgeStorageLevel = StorageLevel.MEMORY_AND_DISK, vertexStorageLevel = StorageLevel.MEMORY_AND_DISK)
-        if (matchedGraph.ops.degrees.aggregate(0)((x, v) => scala.math.max(x, v._2), (x, y) => scala.math.max(x, y)) >= 2) {
-          println("The matched edges do not form a matching.")
-          sys.exit(1)
-        }
-
-        println("The matched edges form a matching of size: " + matchedEdges.count())
-
-      case _ =>
-        println("Invalid command or number of arguments")
+    if (args(0) == "compute") {
+      if (args.length != 3) {
+        println("Usage: GraphMatching compute graph_path output_path")
         sys.exit(1)
+      }
+
+      val startTimeMillis = System.currentTimeMillis()
+
+      val edgesRDD = sc.textFile(args(1)).map(line => {
+        val x = line.split(",")
+        Edge(x(0).toLong, x(1).toLong, 1)
+      })
+
+      val graph = Graph(
+        sc.emptyRDD[(Long, Int)], // Placeholder for vertices
+        edgesRDD,
+        0,  // Default vertex attribute
+        StorageLevel.MEMORY_AND_DISK,  // Edge storage level
+        StorageLevel.MEMORY_AND_DISK  // Vertex storage level
+      )
+
+      val matching = maximalMatching(graph)
+
+      val endTimeMillis = System.currentTimeMillis()
+      val durationSeconds = (endTimeMillis - startTimeMillis) / 1000
+      println(s"Luby's Matching Algorithm completed in $durationSeconds s.")
+
+      saveMatching(matching, args(2))
+    } else if (args(0) == "verify") {
+      if (args.length != 3) {
+        println("Usage: GraphMatching verify graph_path matching_path")
+        sys.exit(1)
+      }
+
+      val edgesRDD = sc.textFile(args(1)).map(line => {
+        val x = line.split(",")
+        Edge(x(0).toLong, x(1).toLong, 1)
+      })
+
+      val verticesRDD = sc.textFile(args(2)).map(line => {
+        val x = line.split(",")
+        (x(0).toLong, x(1).toInt)
+      })
+
+      val graph = Graph(
+        verticesRDD,
+        edgesRDD,
+        0,  // Default vertex attribute
+        StorageLevel.MEMORY_AND_DISK,  // Edge storage level
+        StorageLevel.MEMORY_AND_DISK  // Vertex storage level
+      )
+
+      val isMatching = verifyMatching(graph)
+
+      if (isMatching)
+        println("Yes")
+      else
+        println("No")
     }
   }
+
+  def maximalMatching(graph: Graph[Int, Int]): List[(Long, Long)] = {
+    var matchedVertices = Set[Long]()
+    val edges = graph.edges.collect().toList
+
+    val matching = edges.filter { edge =>
+      val u = edge.srcId
+      val v = edge.dstId
+
+      if (!matchedVertices.contains(u) && !matchedVertices.contains(v)) {
+        matchedVertices += u
+        matchedVertices += v
+        true
+      } else {
+        false
+      }
+    }
+
+    // Return the matching
+    return matching.map(edge => (edge.srcId, edge.dstId))
+  }
+
+  def saveMatching(matching: List[(Long, Long)], outputFile: String) {
+    val spark = SparkSession.builder().getOrCreate()
+    val df = spark.createDataFrame(matching).toDF("u", "v")
+
+    df.coalesce(1).write.format("csv").mode("overwrite").save(outputFile)
+  }
+
+  def verifyMatching(graph: Graph[Int, Int]): Boolean = {
+  val independent = graph.triplets.flatMap { triplet =>
+    if (triplet.srcAttr == 1 && triplet.dstAttr == 1) {
+      Some((triplet.srcId, triplet.dstId))
+    } else None
+  }.count() == 0
+
+  val maximal = graph.vertices.leftOuterJoin(graph.aggregateMessages[Int](
+    triplet => {
+      if (triplet.srcAttr == 1) triplet.sendToDst(1)
+      if (triplet.dstAttr == 1) triplet.sendToSrc(1)
+    },
+    (a, b) => a + b
+  )).map {
+    case (id, (label, Some(count))) => label == 1 || count > 0
+    case (id, (label, None)) => label == 1
+  }.reduce(_ && _)
+
+  // Return the combined result
+  return independent && maximal
+}
 }
